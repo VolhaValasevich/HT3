02f28da361350e5b645885e8999a9ecba7675774
VolhaValasevich <36482820+VolhaValasevich@users.noreply.github.com>
2018-04-05 09:59:14 +0300

    upd

--- GitHubAutomation/src/test/java/com/epam/ta/GitHubAutomationTest.java
+++ GitHubAutomation/src/test/java/com/epam/ta/GitHubAutomationTest.java
@@ -41,8 +41,8 @@ public class GitHubAutomationTest
 	{
 		steps.loginGithub(USERNAME, PASSWORD);
 		steps.createNewRepository("testRepo", "auto-generated test repo");
-		Assert.assertTrue(steps.starCurrentRepository(), "[Failed to star a repository!]");
-		Assert.assertTrue(steps.unstarCurrentRepository(), "[Failed to unstar a repository!]");
+		Assert.assertTrue(steps.starCurrentRepository(), "[Failed to star a repository. The 'Unstar' button does not appear.]");
+		Assert.assertTrue(steps.unstarCurrentRepository(), "[Failed to unstar a repository. The 'Star' button does not appear.]");
 	}
 
 	@Test(description = "Submit new issue")
@@ -50,14 +50,14 @@ public class GitHubAutomationTest
 		steps.loginGithub(USERNAME, PASSWORD);
 		steps.createNewRepository("testRepo", "auto-generated test repo");
 		Assert.assertTrue(steps.submitNewIssue("testIssue", "auto-generatet issue comment"),
-				"[Failed to submit an issue!]");
+				"[Failed to submit an issue.]");
 	}
 
     @Test(description = "Delete repository")
 	public void oneCanDeleteRepository() {
 		steps.loginGithub(USERNAME, PASSWORD);
 		steps.createNewRepository("testRepo", "auto-generated test repo");
-		Assert.assertTrue(steps.deleteRepository(), "[Failed to delete repository!]");
+		Assert.assertTrue(steps.deleteRepository(), "[Failed to delete repository.]");
 	}
 
 	@AfterMethod(description = "Stop Browser")
